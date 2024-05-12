57065a1922d416342995afa24931ca25005fa940
Macdonald Nkhata <nkhatamac27@gmail.com>
2024-05-12 11:37:50 +0200

    fixed the visibility issue of the number on links

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -12,7 +12,7 @@ const Nav = () => {
 
   return (
     <div className="w-full">
-      <nav className="w-full flex flex-row items-center justify-between lg:hidden">
+      <nav className="w-full flex flex-row items-center justify-between lg:hidden md:hidden">
         <div className="relative w-[20%]">
           <Link href="/" className="absolute top-6 left-6">
             <Image
@@ -44,7 +44,9 @@ const Nav = () => {
                 <Link
                   href="/"
                   className={`${
-                    pathname === '/' ? 'border-r-[4px] border-secondary' : ''
+                    pathname === '/'
+                      ? 'border-r-[4px] border-secondary py-2'
+                      : ''
                   }`}
                 >
                   <span className="font-bold">00</span> Home
@@ -53,7 +55,7 @@ const Nav = () => {
                   href="/destination"
                   className={`${
                     pathname === '/destination'
-                      ? 'border-r-[5px] border-secondary mt-8'
+                      ? 'border-r-[5px] border-secondary py-2 mt-8'
                       : 'mt-8'
                   }`}
                 >
@@ -63,7 +65,7 @@ const Nav = () => {
                   href="/crew"
                   className={`${
                     pathname === '/crew'
-                      ? 'border-r-[5px] border-secondary mt-8'
+                      ? 'border-r-[5px] border-secondary py-2 mt-8'
                       : 'mt-8'
                   }`}
                 >
@@ -73,7 +75,7 @@ const Nav = () => {
                   href="/technology"
                   className={`${
                     pathname === '/technology'
-                      ? 'border-r-[5px] border-secondary mt-8'
+                      ? 'border-r-[5px] border-secondary py-2 mt-8'
                       : 'mt-8'
                   }`}
                 >
@@ -84,6 +86,64 @@ const Nav = () => {
           )}
         </div>
       </nav>
+
+      <div className="hidden lg:w-full lg:h-[96px] lg:mx-auto lg:flex lg:mt-10 md:flex md:mt-0 md:h-[80px] justify-between items-center">
+        <Link href="/" className="cursor-pointer ml-12">
+          <Image
+            src="/images/shared/logo.svg"
+            alt="logo"
+            width={48}
+            height={48}
+            priority
+          />
+        </Link>
+        <nav className="hidden  h-full bg-gray-900 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-30 items-center lg:w-[60%] lg:flex md:w-[60%] md:flex">
+          <div className="flex justify-between w-[50%] ml-32 text-base font-sans uppercase text-accent tracking-widest md:ml-16 md:w-3/4">
+            <Link
+              href="/"
+              className={`${
+                pathname === '/'
+                  ? 'border-b-[3px] border-text-accent pb-6 mt-10 md:mt-8'
+                  : 'mt-10 md:mt-8'
+              }`}
+            >
+              <span className="lg:font-bold lg:inline md:hidden">00</span> Home
+            </Link>
+            <Link
+              href="/destination"
+              className={`${
+                pathname === '/destination'
+                  ? 'border-b-[3px] border-text-accent pb-6 mt-10 md:mt-8'
+                  : 'mt-10 md:mt-8'
+              }`}
+            >
+              <span className="lg:font-bold lg:inline md:hidden">01</span>{' '}
+              Destination
+            </Link>
+            <Link
+              href="/crew"
+              className={`${
+                pathname === '/crew'
+                  ? 'border-b-[3px] border-text-accent pb-6 mt-10 md:mt-8'
+                  : 'mt-10 md:mt-8'
+              }`}
+            >
+              <span className="lg:font-bold lg:inline md:hidden">02</span> Crew
+            </Link>
+            <Link
+              href="/technology"
+              className={`${
+                pathname === '/technology'
+                  ? 'border-b-[3px] border-text-accent pb-6 mt-10 md:mt-8'
+                  : 'lg:mt-10 md:mt-8'
+              }`}
+            >
+              <span className="lg:font-bold lg:inline md:hidden">03</span>{' '}
+              Technology
+            </Link>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 };
